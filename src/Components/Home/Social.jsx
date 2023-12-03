823af21db2a6bfd7de4d9e9ff9b297285183d03e
pradipkaityuiux <pradipforeducation@gmail.com>
2023-12-03 18:59:42 +0530

    Header Created

---
+++ src/Components/Home/Social.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Social() {
+  return (
+    <div className="home__social">
+        <a href="" className="home__social-icon" target='_blank'><i class="uil uil-instagram"></i></a>
+        <a href="" className="home__social-icon" target='_blank'><i class="uil uil-dribbble"></i></a>
+        <a href="" className="home__social-icon" target='_blank'><i class="uil uil-github-alt"></i></a>
+    </div>
+  )
+}
+
+export default Social
