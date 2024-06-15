2f20df19c1af414c687685f6027f61193cbb4fdf
pradipkaityuiux <pradipforeducation@gmail.com>
2024-06-15 08:06:08 +0530

    Auto scroll functionallity added

--- src/hooks/CommonSec.jsx
+++ src/hooks/CommonSec.jsx
@@ -2,13 +2,14 @@ import React, { useRef } from 'react';
 import { motion } from 'framer-motion';
 import { useInView } from './useInView';
 
-const Section = ({ children }) => {
+const Section = ({ children, id }) => {
   const ref = useRef();
   const isInView = useInView(ref, { threshold: 0.3 });
 
   return (
     <motion.div
       ref={ref}
+      id={id}
       initial={{ opacity: 0, y: 50 }}
       animate={isInView ? { opacity: 1, y: 0 } : {}}
       transition={{ duration: 0.8, ease: 'easeOut' }}
